1f3533d8fdb0bd9c11d4c8c7498e9cf6e99b740e
Sion Park <bgrrt3661@gmail.com>
2023-06-30 10:31:10 +0900

    delete video.js

--- routes/video.js
+++
@@ -1,8 +0,0 @@
-// 구체적인 영상을 선택해서 영상 시청화면으로 넘어갈 때를 정의하기 위한 경로입니다
-// 아직 미완성 입니다 프론트 쪽에서 영상재생 화면을 보내주시면 그때 작성할까 합니다
-
-const express = require("express");
-
-const router = express.Router();
-
-module.exports = router;
